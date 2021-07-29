196c6a6ae0c45c8a8431fbc2fe3e37d1738e5cff
DarioDa86 <dario.dagnelli@gmail.com>
2021-07-29 20:44:30 +0200

    inseriti contenuti grafici, da fixare

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 //1- richiedere numero di km da percorrere
 var distanzaKm = parseInt(prompt("Inserisci i Km che devi percorrere"));
+document.getElementById('distanzaKm').innerHTML += distanzaKm;
+
 //2- richiedere età passeggero
 var etaPasseggero = parseInt(prompt("Inserisci la tua età"));
+document.getElementById('etaPasseggero').innerHTML += etaPasseggero;
+
 //3- determinare quanto costa il biglietto, seguendo queste regole:
 //0,21 cent per 1km
 var costoKm = 0.21;
 console.log(costoKm);
 
-totaleParziale = distanzaKm * costoKm;
+var totaleParziale = distanzaKm * costoKm;
 
 // sconto 20% per minorenni
 sconto20 = ((totaleParziale / 100) * 20);
@@ -21,18 +25,23 @@ console.log(sconto40);
 
 if (etaPasseggero <= 17) {
     var totale =  totaleParziale - sconto20;
-    alert(totale + " €");
+    alert(totale.toFixed(2) + " €");
+    document.getElementById('totale').innerHTML += totale;
 
 }
 
 else if (etaPasseggero >= 65) {
     var totale =  totaleParziale - sconto40 ;
-    alert(totale + " €");
+    alert(totale.toFixed(2) + " €");
+    document.getElementById('totale').innerHTML += totale;
+
 
 }
 
 else {
-    alert(totaleParziale + " €");
+    alert(totaleParziale.toFixed(2) + " €");
+    document.getElementById('totale').innerHTML += totaleParziale;
+
 }
 
 
